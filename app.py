ad98d41aee45fd2a7403f9b70d3a9f0f2f2cbe03
Raushan kumar <raushankumar321670@gmail.com>
2026-08-31 13:42:23 +0530

    refactor: clean up Streamlit recommender app

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-import streamlit as st
 import pickle
+
 import pandas as pd
-import requests
+import streamlit as st
 
 
-def recommend(movie):
-    movie_index=movies[movies['title']==movie].index[0]
-    distances=similarity[movie_index]
-    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+def recommend(movie_title: str) -> list[str]:
+    """Return the five most similar movie titles for the selected movie."""
+    movie_index = movies[movies["title"] == movie_title].index[0]
+    distances = similarity[movie_index]
+    ranked_movies = sorted(
+        enumerate(distances),
+        reverse=True,
+        key=lambda item: item[1],
+    )[1:6]
 
-    recommended_movie=[]
-    for i in movies_list:
-        for i in movies_list:
-           movie_id=i[0]
-           # fetch poster from api
-
-        recommended_movie.append(movies.iloc[i[0]].title)
-    return recommended_movie
+    return [movies.iloc[index].title for index, _ in ranked_movies]
 
 
+with open("similarity.pkl", "rb") as file:
+    similarity = pickle.load(file)
 
-similarity=pickle.load(open('similarity.pkl','rb'))
+with open("movie_dict.pkl", "rb") as file:
+    movie_dict = pickle.load(file)
 
-movie_dict=pickle.load(open('movie_dict.pkl','rb'))
-movies=pd.DataFrame(movie_dict)
+movies = pd.DataFrame(movie_dict)
 
-st.title('Movie Recommender System')
+st.title("Movie Recommender System")
 
-selected_movie_name=st.selectbox(
-    'How Would you like to be contacted?',movies['title'].values)
+selected_movie_name = st.selectbox(
+    "Select a movie",
+    movies["title"].values,
+)
 
-if st.button('Recommend'):
-   recommendations= recommend(selected_movie_name)
-   for i in recommendations:
-    st.write(i)
+if st.button("Recommend"):
+    for recommendation in recommend(selected_movie_name):
+        st.write(recommendation)
